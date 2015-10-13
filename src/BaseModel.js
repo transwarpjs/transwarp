8a142d6814a9e1b7f111d34bac5f0e0e9986761d
fundon <cfddream@gmail.com>
2015-10-14 07:15:56 +0800

    BaseModel

--- src/BaseModel.js
+++ src/BaseModel.js
@@ -1,4 +1,31 @@
+import EventEmitter from 'events'
+import _ from 'lodash'
+import { plural, singular } from 'pluralize'
 
 export default class BaseModel {
 
+  static schema = Object.create(null)
+
+  static create(attributes) {
+    const m = new this(attributes)
+    return m
+  }
+
+  static get type() {
+    return plural(this.name.toLowerCase())
+  }
+
+  // User -> users
+  static get defaultTableName() {
+    return plural(this.name.toLowerCase())
+  }
+
+  // eg. custon table name
+  // static tableName = 'users'
+  static get tableName() {
+    return this.defaultTableName
+  }
+
+  // eg. postgres
+  static get schemaName() {}
 }
