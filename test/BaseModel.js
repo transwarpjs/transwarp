532bc1600cc31eb4802d82fe418e1826d809d93e
fundon <cfddream@gmail.com>
2015-10-16 06:57:26 +0800

    test: update BaseModel

--- test/BaseModel.js
+++ test/BaseModel.js
@@ -9,6 +9,33 @@ describe('BaseModel', () => {
 
       assert.equal(BaseModel.create().constructor, BaseModel)
       assert.equal(BaseModel.create() instanceof BaseModel, true)
+      assert.equal(BaseModel.create().constructor, (new BaseModel).constructor)
+
+    })
+
+  })
+
+  describe('#defaultTableName', () => {
+
+    it('should get a table name by default', () => {
+
+      assert.equal(BaseModel.defaultTableName, 'basemodels')
+
+    })
+
+  })
+
+  describe('#tableName', () => {
+
+    it('shoudl equal to default table name', () => {
+
+      assert.equal(BaseModel.tableName, BaseModel.defaultTableName)
+
+    })
+
+    it('should get a table name', () => {
+
+      assert.equal(BaseModel.tableName, 'basemodels')
 
     })
 
