ee046aed54c72b1c003b9f486d5862c6c0728332
fundon <cfddream@gmail.com>
2015-10-18 20:06:44 +0800

    add Scope

---
+++ src/Scope.js
@@ -0,0 +1,55 @@
+
+export default class Scope {
+
+  // The row sql
+  sql = ''
+  values = null
+
+  constructor({ db }) {
+    this.db = db
+  }
+
+  // Gets dialect
+  get dialect() {
+    return this.db.connector.dialect
+  }
+
+  // Shorts for the db connection
+  get conn() {
+    return this.db.connection
+  }
+
+  row(sql = '') {
+    //const reg = new RegExp('\$\$', 'g')
+    //this.sql = sql.replace(reg, '?')
+    return this
+  }
+
+  build(sql, values) {
+    this.sql = sql
+    this.values = values
+    return this
+  }
+
+  /**
+   * Executes invoke sql
+   */
+  exec() {
+    return this.conn
+      .then(({ client, done }) => {
+        return new Promise((resolve, reject) => {
+          client.query(this.sql, this.values, (err, result) => {
+            // release pool conn
+            done(err)
+
+            // error
+            if (err) return reject(err)
+
+            // response
+            resolve(result)
+          })
+        })
+      })
+  }
+
+}
